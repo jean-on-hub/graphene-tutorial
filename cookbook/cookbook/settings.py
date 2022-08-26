a9feb282b07d5fb4780da293556cb3952e4e5362
jean-on-hub <87473040+jean-on-hub@users.noreply.github.com>
2022-08-26 12:43:05 +0000

    secret key

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p1cj2atxm-ui-__&k^8y#pwvv=h!+cdj0!py6&wo@x$28+&qfi"
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
